3d3d54d397f5ee4ab593862ebd060b69a96e7071
Arkajit <arkajitmandal@rocheste.edu>
2017-10-03 19:14:27 -0400

    setup.py written properly

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='Langevin Dynamics Simulator',
+      version='0.0',
+      description='A 1D langevin simulator',
+      author='Arkajit Mandal',
+      author_email='arkajitmandal@rochester.edu",
+      url='https://github.com/arkajitmandal',
+      packages=["LangevinDynamics"],
+      entry_points = 
+      {
+          'console_scripts' : ['langevin = LangevinDynamics.start:go']
+          }
+      )
+
+    
