0f9faa5c3c7ef16a21f98aeb2c99f8ada16105ac
Arkajit <arkajitmandal@rocheste.edu>
2017-10-03 23:38:11 -0400

    some minor modification

--- LangevinDynamics/__init__.py
+++ LangevinDynamics/__init__.py
@@ -1,1 +1,18 @@
 """This is the code for Langevin Dynamics"""
+import numpy as np 
+
+def read(filename):
+    """
+    reads a text file with space separated values
+    it assumes first column as x and second column as V(x)
+    returns a 2D numpy array 
+    """
+    fob = open(filename,"r")
+    dat = fob.readlines()
+    lines = []
+    for i in dat:
+        lines+=i.replace("\n")
+    Vx = np.array(lines,"float")    
+    Vx.shape = (len(dat),2)
+    return Vx 
+
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(name='Langevin Dynamics Simulator',
-      version='0.0',
+      version='0.01',
       description='A 1D langevin simulator',
       author='Arkajit Mandal',
-      author_email='arkajitmandal@rochester.edu",
+      author_email="arkajitmandal@rochester.edu",
       url='https://github.com/arkajitmandal',
       packages=["LangevinDynamics"],
-      entry_points = 
-      {
+
+      entry_points = { 
           'console_scripts' : ['langevin = LangevinDynamics.start:go']
           }
       )
