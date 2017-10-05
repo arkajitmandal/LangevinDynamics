6d1b2a61e36e43a61cb2460c013b84a13214d190
Arkajit <arkajitmandal@rocheste.edu>
2017-10-04 20:53:30 -0400

    coverage img automated

--- LangevinDynamics/__init__.py
+++ LangevinDynamics/__init__.py
@@ -1,19 +1,19 @@
 """This is the code for Langevin Dynamics"""
 import numpy as np 
 
-def read(filename):
+def read(fob):
     """
     reads a text file with space separated values
     it assumes first column as x and second column as V(x)
     returns a 2D numpy array 
     """
-    fob = open(filename,"r")
     dat = fob.readlines()
+    print(dat)
     lines = []
     for i in dat:
-        lines+=i.replace("\n").split()
-        
-    Vx = np.array(lines,"float")    
-    Vx.shape = (len(dat),len(i))
-    return Vx 
+        lines+=i.replace("\n","").split()
+    Column =  len(i.replace("\n","").split())    
+    Data = np.array(lines,"float")    
+    Data.shape = (len(dat),Column)
+    return Data 
 
--- test/test.py
+++ test/test.py
@@ -1,3 +1,12 @@
+import numpy
+import io
 def test_import():
     import LangevinDynamics
+def test_read():
+    import LangevinDynamics as ld
+    mytxt = u"""1 0.1 5 5
+    2 0.2 6 6"""
+    myFile = io.StringIO(mytxt)
+    dat = ld.read(myFile)
+    assert (dat[0][0] == 1.0) , "data not read properly" 
 
