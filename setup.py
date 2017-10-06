50e7ea711a9b22a1aad62ddcd392c6fc09a41adf
Arkajit <arkajitmandal@rocheste.edu>
2017-10-06 11:29:30 -0400

    setup.py improvements

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from distutils.core import setup
 
+with open('README.md') as f:
+        long_description = ''.join(f.readlines())
+
+with open('requirements.txt') as f:
+        all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x] 
+
 setup(name='Langevin Dynamics Simulator',
-      version='0.01',
+      version='0.02',
       description='A 1D langevin simulator',
+      long_description=long_description,
       author='Arkajit Mandal',
       author_email="arkajitmandal@rochester.edu",
       url='https://github.com/arkajitmandal',
       packages=["LangevinDynamics"],
-
+      install_requires=install_requires,
       entry_points = { 
           'console_scripts' : ['langevin = LangevinDynamics.start:go']
           }
