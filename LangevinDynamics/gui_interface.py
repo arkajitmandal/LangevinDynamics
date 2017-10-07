532a2721c02b814797b2bce9f616c25b2aed2ed2
Arkajit <arkajitmandal@rocheste.edu>
2017-10-06 21:43:42 -0400

    success message in gui added

--- LangevinDynamics/gui_interface.py
+++ LangevinDynamics/gui_interface.py
@@ -36,8 +36,9 @@ def callback(element):
             break
     if go :
         import LangevinDynamics as ld
-     
         ld.run(float(val[0]),float(val[1]),float(val[2]),float(val[3]),float(val[4]),val[-1],float(val[5]),int(val[6]),str(val[7]))
+        messagebox.showinfo("Simulation Complete", "output saved as %s"%(val[7]))
+
 
 def start():
     element = ["x","p","m","Temperature","lambda","dt","steps","output","filename"]
