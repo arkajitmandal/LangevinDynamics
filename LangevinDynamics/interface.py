4448b83ec9a4ec02437417eeb449feb32d9b8015
Arkajit <arkajitmandal@rocheste.edu>
2017-10-07 10:30:54 -0400

    console error for v and p fixed

--- LangevinDynamics/interface.py
+++ LangevinDynamics/interface.py
@@ -2,7 +2,7 @@ import os
 import click
 @click.command()
 @click.option('--x', prompt='Initial Position, x', help='Input initial position')
-@click.option('--v', prompt='Initial Momentum, p', help='Input initial velocity')
+@click.option('--v', prompt='Initial velocity, v', help='Input initial velocity')
 @click.option('--temp', prompt='Temperature, T', help='Input Temperature')
 @click.option('--lamda', prompt='Damping Coefficient , lambda', help='Input damping coefficient')
 @click.option('--m', prompt='mass, m', help='Input mass')
@@ -10,7 +10,7 @@ import click
 @click.option('--steps', prompt='number of steps, N', help='Input timestep for the simulation')
 @click.option('--o', default="out.dat" , help='Name of the output file')
 @click.option('--i', prompt='Input file' , help='Name of the input file')
-def start(x,p,temp,lamda,m,dt,steps,o,i):
+def start(x,v,temp,lamda,m,dt,steps,o,i):
     import LangevinDynamics as ld
     i = os.getcwd()+"/" + i
     ld.run(float(x),float(v)*float(m),float(m),float(temp),float(lamda),i,float(dt),int(steps),o)
